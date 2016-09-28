d272dedb603416411986f96661c349f40f3ba2df
DCSaunders <ds636@cam.ac.uk>
2016-09-29 00:06:20 +0100

    ex 2.4: squeeze

--- 2_Types_Operators_Expressions/2.4.squeeze.c
+++ 2_Types_Operators_Expressions/2.4.squeeze.c
@@ -9,12 +9,12 @@ int main()
   int c, i;
   char s1[MAXLINE];
   char s2[MAXLINE];
-  for (i=0; (i < MAXLINE - 1) && (c=getchar()) != EOF && c!= '\n'; ++i)
+  for (i=0; (i < MAXLINE - 1) && (c=getchar()) != EOF && c!= '\n'; i++)
     {
       s1[i] = c;
     }
   s1[i] = '\0';
-  for (i = 0; (i < MAXLINE - 1) && (c=getchar()) != EOF && c!= '\n'; ++i)
+  for (i = 0; (i < MAXLINE - 1) && (c=getchar()) != EOF && c!= '\n'; i++)
     {
       s2[i] = c;
     }
@@ -27,20 +27,22 @@ int main()
 
 void squeeze(char s1[], char s2[])
 {
-  char s2_chars[CHAR_MAX];
-    int i, j;
+  char s2_chars[CHAR_MAX + 1];
+  s2_chars[CHAR_MAX] = '\0';
+  int i, j;
   i = j = 0;
   while (s2[i] != '\0')
     {
-      s2_chars[s2[i++]] = 1;
+      s2_chars[s2[i++]] = '1';
     }
-  i = 0;
+  i = 0; 
   while (s1[i] != '\0')
     {
-      if (s2_chars[s1[i]] != 1)
+      if (s2_chars[s1[i]] != '1')
 	{
-	  s1[j++] = s1[i++];
+	  s1[j++] = s1[i];
 	}
+      i++;
     }
   s1[j] = '\0';
 }
