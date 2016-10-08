ada54c81dd29616d6e0575a95a44643803ac1388
DCSaunders <ds636@cam.ac.uk>
2016-10-08 23:16:12 +0100

    ex 3.2 escape

--- 3_Control_Flow/3.2.escape.c
+++ 3_Control_Flow/3.2.escape.c
@@ -13,13 +13,60 @@ int main()
       s[len] = c;
     }
   s[len] = '\0';
-  printf("%s", s);
+  escape(s, t);
+  printf("%s", t);
   return 0;
 }
 
 void escape(char s[], char t[])
 {
-}  
+  int i, j;
+  i = j = 0;
+  while (s[i] != '\0')
+    {
+      switch (s[i])
+	{
+	case '\n':
+	  t[j++] = '\\';
+	  t[j] = 'n';
+	  break;
+	case '\t':
+	  t[j++] = '\\';
+	  t[j] = 't';
+	  break;
+	default:
+	  t[j] = s[i];
+	}
+      i++;
+      j++;
+    }
+  t[j] = '\0';
+}
 void rescape(char s[], char t[])
 {
+  int i, j;
+  i = j = 0;
+  while (s[i] != '\0')
+    {
+      switch (s[i])
+	{
+	case '\\':
+	  switch(s[i+1])
+	    {
+	    case 'n':
+	      t[j] = '\n';
+	      i++;
+	      break;
+	    case 't':
+	      t[j] = '\t';
+	      i++;
+	      break;
+	    }
+	default:
+	  t[j] = s[i];
+	}
+      i++;
+      j++;
+    }
+  t[j] = '\0';
 }
