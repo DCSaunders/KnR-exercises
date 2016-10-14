7988262e4bc1a1f6a413c371812d370511788f8a
DCSaunders <ds636@cam.ac.uk>
2016-10-14 23:38:11 +0100

    ex 5: base b representation of int

--- 3_Control_Flow/3.4-6.itoaba.c
+++ 3_Control_Flow/3.4-6.itoaba.c
@@ -5,13 +5,14 @@
 /*The original itoa function does not handle the largest negative number. This is because the number is first made positive (if negative) by multiplying by sign. |largest possible negative int| > |largest positive int|, so this would fail according to the values in limits.h (-2147483648)*/
 
 void itoa(int n, char s[]);
+void itob(int n, char s[], int b);
 void reverse(char s[]);
 
 int main()
 {
   char s[MAXLEN];
-  int n = -2147483648;
-  itoa(n, s);
+  int n = 31;
+  itob(n, s, 16);
   printf("%s\n", s);
   return 0;
 }
@@ -24,6 +25,23 @@ void itoa(int n, char s[])
   do {
     s[i++] = sign * (n % 10) + '0';
   } while (sign * (n /= 10) > 0);
+  if (sign < 0)
+    s[i++] = '-';
+  s[i] = '\0';
+  reverse(s);
+}
+
+
+void itob(int n, char s[], int b)
+{
+  int i, sign, digit;
+  sign = (n < 0) ? -1 : 1;
+  i = 0;
+  do {
+    digit = sign * (n % b);
+    digit += (digit > 9)? ('A' - 10) : '0'; 
+    s[i++] = digit;
+  } while (sign * (n /= b) > 0);
   if (sign < 0)
     s[i++] = '-';
   s[i] = '\0';
