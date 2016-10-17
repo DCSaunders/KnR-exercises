409a1e5d13a6bf5f73258f48475977e3af362383
DCSaunders <ds636@cam.ac.uk>
2016-10-17 23:41:45 +0100

    ex 4.1: rightmost index of substr

--- 4_Functions_Program_Structure/4.1.strright.c
+++ 4_Functions_Program_Structure/4.1.strright.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #define MAXLINE 1000
 
-int getline(char line[], int lim);
+int getline_new(char line[], int lim);
 int strright(char source[], char searchfor[]);
 
 int main()
@@ -9,18 +10,20 @@ int main()
   char pattern[] = "ould";
   char line[MAXLINE];
   int found = 0;
-  while(getline(line, MAXLINE) > 0)
+  int index;
+  while(getline_new(line, MAXLINE) > 0)
     {
-      if (strright(line, pattern) >= 0)
+      index = strright(line, pattern);
+      if (index >= 0)
 	{
-	  printf("%s\n", line);
+	  printf("%s %d\n", line, index);
 	  found++;
 	}
     }
   return found;
 }
 
-int getline(char line[], int lim)
+int getline_new(char line[], int lim)
 {
   int c, i;
   i = 0;
@@ -35,6 +38,25 @@ int getline(char line[], int lim)
 int strright(char source[], char searchfor[])
 {
   int index = -1;
-
-  
+  int sourcelen = strlen(source);
+  int searchlen = strlen(searchfor);
+  int j = searchlen - 1;
+  for (int i = sourcelen - 1; i>=0; i--)
+    {
+      if (source[i] != searchfor[j])
+	{
+	  if (j < (searchlen - 1))
+	  j = searchlen - 1;
+	}
+	else if (j > 0)
+	  {
+	    j--;
+	  }
+	else if (j == 0)
+	  {
+	    index = i;
+	    break;
+	  }
+    }
+  return index;
 }
